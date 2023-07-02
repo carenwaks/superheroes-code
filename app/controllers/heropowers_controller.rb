d33feaf414bf4e94590d7ca66565dd05ff9291c3
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-07-03 00:27:50 +0300

    updated heropower controller

--- app/controllers/heropowers_controller.rb
+++ app/controllers/heropowers_controller.rb
@@ -1,15 +1,15 @@
 class HeropowersController < ApplicationController
-    def index
-        heropowers = Heropower.all
-        render json: heropowers
-    end
+    # def index
+    #     heropowers = Heropower.all
+    #     render json: heropowers
+    # end
 
     def create
         heropower = Heropower.create(heropower_params)
         if heropower.save
             render json: heropower, status: :created
         else
-            render json: { "errors":  }, status: :unprocessable_entity
+            render json: { "errors": hero_power.errors.full_messages}, status: :unprocessable_entity
         end
     end
 
